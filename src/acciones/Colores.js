d244ad6d79057801696b570e7e228e21a84ff397
emeery <gerardoemery@live.com.mx>
2018-04-15 16:31:57 -0500

    info p.

--- src/acciones/Colores.js
+++ src/acciones/Colores.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function cargaColor() {
     return(dispatch)=>{
-        return axios.get("http://www.colr.org/json/color/random").then((response)=>{
+        return axios.get("https://www.colr.org/json/color/random").then((response)=>{
             dispatch(cambiaColor('#'+response.data.new_color));
        });
     }
