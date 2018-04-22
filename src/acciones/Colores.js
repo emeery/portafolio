13f1bbc8c05a9c534b26aedbf37be5dd68918daf
emeery <gerardoemery@live.com.mx>
2018-04-21 20:48:45 -0500

    random

--- src/acciones/Colores.js
+++ src/acciones/Colores.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function cargaColor() {
     return(dispatch)=>{
-        return axios.get("http://www.colr.org/json/color/random")
+        return axios.get("//www.colr.org/json/color/random")
         .then((response, err)=>{
             dispatch(cambiaColor('#'+ response.data.new_color));
        }).catch((err) => {
