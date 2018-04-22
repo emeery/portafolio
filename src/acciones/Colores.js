894c8ac97e5dd30a58636e4ff7397e2d2cd25bd4
emeery <gerardoemery@live.com.mx>
2018-04-21 21:36:16 -0500

    fetch colores

--- src/acciones/Colores.js
+++ src/acciones/Colores.js
@@ -1,5 +1,26 @@
 import axios from 'axios';
 
+// cargaColor
+export default (dispatch) => {
+    fetch("//www.colr.org/json/color/random")
+    .then( res => {
+        dispatch(cambiaColor('#'+ response.data.new_color));
+    })
+    // return(dispatch)=>{
+    //     return axios.get("//www.colr.org/json/color/random")
+    //     .then((response, err)=>{
+    //         dispatch(cambiaColor('#'+ response.data.new_color));
+    //    }).catch((err) => {
+    //     console.log('hubo error', err);
+    //    });
+    // }
+}
+// switch(action.type) {}
+export const cambiaColor = (color) => ({
+    type: 'CAMBIA_COLOR',
+    color
+});
+
 export function cargaColor() {
     return(dispatch)=>{
         return axios.get("//www.colr.org/json/color/random")
@@ -10,8 +31,3 @@ export function cargaColor() {
        });
     }
 }
-// switch(action.type) {}
-export const cambiaColor = (color) => ({
-    type: 'CAMBIA_COLOR',
-    color
-});
