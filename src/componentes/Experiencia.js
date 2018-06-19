063834270cd7a888e7d16aae988410f8a0f55c3d
emeery <gerardoemery@live.com.mx>
2018-06-19 02:18:53 -0500

    jpg

--- src/componentes/Experiencia.js
+++ src/componentes/Experiencia.js
@@ -1,70 +1,63 @@
 import React from 'react';
 
 class Experiencia extends React.Component {
-    
-    state = { 
-      empresa: 'BBVA',
-      posicion: 'ANALISTA/PROGRAMADOR, FRONT-END',
-      periodo: 'JUNIO/17 - ENERO/2018'
-    }
-   
-    render() {
+     render() {
         return (
-<div className="card-deck">
-    <div className="card">
-    
-      <div className="card-body">
-              <center>
-              <i className="far fa-building fa-5x"></i>
-              </center>
-            <h5 className="card-title">Empleo</h5>
-            <p className="card-text"> 
-              {this.state.empresa} </p>
-            <p className="card-text"> 
-              {this.state.posicion}</p>
-            <p className="card-text"> 
-              {this.state.periodo}</p>
-      </div>
-    </div>
-  
-    <div className="card">
+    <div className="card-deck">
       
-      <div className="card-body">
-              <center>
-                <i className="fa fa-folder-open fa-5x" aria-hidden="true"></i>
-              </center>
-            <h5 className="card-title"> Lenguajes</h5>
-            <p className="card-text"> 
-              JAVASCRIPT (<span>ES6</span>) - JAVA - 
-            </p>
-      
-            <p className="card-text"> 
-              REACT-JS(<span>REDUX, WEBPACK, SASS</span>)
-            </p>
+        <div className="card">
+          
+          <div className="card-body">
+                  <center>
+                    <i className="fa fa-folder-open fa-5x" aria-hidden="true"></i>
+                  </center>
+                <h5 className="card-title"> Lenguajes</h5>
+                <p className="card-text"> 
+                  JAVASCRIPT (<span>ES6</span>) - JAVA - 
+                </p>
+          
+                <p className="card-text"> 
+                  REACT-JS(<span>REDUX, WEBPACK, SASS</span>)
+                </p>
 
-            <p className="card-text"> 
-             MySQL - NoSQL
-            </p>
-    
-      </div>
-    </div>
-  
-  
-    <div className="card">
-      <div className="card-body">
-                <center>
-                  <i className="fa fa-flask fa-5x" aria-hidden="true"></i>
-                </center>
-          <h5 className="card-title"> Herramientas</h5>
-          <p className="card-text">GIT, BASH, HEROKU, VS CODE</p>
-          <p className="card-text">INGLÉS : TÉCNICO</p>
-          
-      </div>
-    </div>
-      
+                <p className="card-text"> 
+                MySQL - NoSQL
+                </p>
         
+          </div>
+        </div>
+      
+      
+        <div className="card">
+          <div className="card-body">
+                    <center>
+                      <i className="fa fa-flask fa-5x" 
+                      aria-hidden="true"></i>
+                    </center>
+              <h5 className="card-title"> Herramientas</h5>
+              <p className="card-text">GIT, BASH, HEROKU, VS CODE</p>
+              <p className="card-text">INGLÉS : TÉCNICO</p>
+              
+          </div>
+        </div>
           
-       </div> 
+        <div className="card">
+          <div className="card-body">
+                    <center>
+                    <i class="fa fa-code fa-5x"></i>
+                    </center>
+              <h5 className="card-title"> Proyectos </h5>
+              <p className="card-text">BUSCADOR YOUTUBE CON API</p>
+              <center>
+              <a 
+              style={{color:'lightblue'}}
+              href="https://buscador123.herokuapp.com/" 
+              target="_blank"
+              className="card-text">Buscador </a>
+              </center>
+          </div>
+        </div>
+    </div> 
         );
     }
 }; export default Experiencia;
